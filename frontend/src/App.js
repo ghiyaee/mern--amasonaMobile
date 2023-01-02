712c8638128ba522d70eaa593d56637d8b5898e0
ghiyaee <86221396+ghiyaee@users.noreply.github.com>
2023-01-02 18:04:08 +0330

    create react

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+// import logo from './logo.svg';
+// import './App.css';
+
+function App() {
+  return (
+    <div dir='rtl'>
+      <header > 
+         <a href="/"> Amasone Mobile</a>
+      </header>
+      <main> لیست محصولات</main>
+    </div>
+  );
+}
+
+export default App;
