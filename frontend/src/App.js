46998d4d9a721eedd7ebff6c1ed60830ca89e85f
ghiyaee <86221396+ghiyaee@users.noreply.github.com>
2023-01-02 23:17:38 +0330

    add products

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,35 @@
 // import logo from './logo.svg';
 // import './App.css';
-
-function App() {
+import data from './data';
+const App = () => {
   return (
-    <div dir='rtl'>
-      <header > 
-         <a href="/"> Amasone Mobile</a>
+    <div dir="rtl">
+      <header>
+        <a href="/"> Amasona Mobile</a>
       </header>
-      <main> لیست محصولات</main>
+      <main>
+        <h1>محصولات ویژه</h1>
+        <div className="products">
+          {data.products.map((p) => (
+            <div className="product" key={p.name}>
+              <a href="/product">
+                <img src={p.image} alt={p.name} />
+              </a>
+
+              <div className="product-info">
+                <p>{p.name}</p>
+                <p>
+                  <strong>{p.price}</strong>
+                  &nbsp;
+                    تومان 
+                </p>
+                <button>افزودن به سبد خرید</button>
+              </div>
+            </div>
+          ))}
+        </div>
+      </main>
     </div>
   );
-}
-
+};
 export default App;
